9679205d4b19dcab541849566570909b90216195
YaliZhang98 <70642339+YaliZhang98@users.noreply.github.com>
2024-09-09 10:08:34 +0200

    Update soil_make.R

--- Soil/Scripts/soil_make.R
+++ Soil/Scripts/soil_make.R
@@ -26,7 +26,6 @@ for (i in 28:538){
   SoilData[,i] <- (SoilData[,i]/10000)
 }
 
-
 ### Get GIT's ###
 featureElim <- species_select(x = SoilData[,c(28:538)],y = SoilData$country,remove_correlated = F,subsets = c(20,30,50,100,200),cores = 8)
 optVars <- featureElim$optVariables
@@ -38,7 +37,7 @@ dir.create('Soil/Outputs', showWarnings = FALSE)
 write.csv(v, file = "Soil/Outputs/soil_git.csv")
 
 
-### Make predictions ###
+### Make predictions (without SMOTE) ###
 coastlines <- cbind("x"  = maps::SpatialLines2map(rworldmap::coastsCoarse)$x ,"y" =maps::SpatialLines2map(rworldmap::coastsCoarse)$y)
 coastlines <- coastlines[complete.cases(coastlines),]
 coastlines <- coastlines[coastlines[,1] < 180 ,]
@@ -59,8 +58,6 @@ for (i in 1:5){
   
 }
 
-
-
 add_preds <- list()
 for (i in 1:5){
   
@@ -68,10 +65,77 @@ for (i in 1:5){
                           "countryPred"= GeoPredsSoil[[i]][[1]], 
                           "latPred" = GeoPredsSoil[[i]][[2]], 
                           "longPred" = GeoPredsSoil[[i]][[3]] )
-  
-  
-  
 }
 
 SoilDataPreds <- rbind.fill(add_preds)
 write.csv(SoilDataPreds,"Soil/Outputs/soil_results.csv")
+
+
+
+### Make predictions (with SMOTE) ###
+library(smotefamily)
+library(dplyr)
+
+SoilData$country <- as.character(SoilData$country)
+SoilData[SoilData$latitude > 60,'country'] <- 'USA_AK'
+
+SoilData[SoilData$longitude < -150,'country'] <- 'USA_HI'
+
+SoilData_smote <- SoilData[,c(optVars,'latitude','longitude','country','continent')]
+SoilData_smote$original <- 1
+SoilData_smote$country <- factor(SoilData_smote$country)
+
+for (c in levels(SoilData_smote$country)){
+  continent_c <- unique(SoilData_smote[SoilData_smote$country == c,'continent'])
+  for (j in 1:nrow(SoilData_smote)){
+    SoilData_smote$target[j] <- ifelse(SoilData_smote$country[j] == c,1,0)
+  }
+
+  # oversampling by SMOTE for countries/sites with less than 4 samples
+  c_samp <- sum(SoilData_smote$country == c)
+    if (c_samp < 4){
+      k = c_samp-1
+    }else{
+      k = 3
+    }
+    
+    dup_size = round(10/c_samp)
+    
+    smote_result.T2 = SMOTE(SoilData_smote[,c(optVars,'latitude','longitude')],target = SoilData_smote$target, K = k, dup_size = dup_size)
+
+    TS2<- smote_result.T2[["syn_data"]]
+    TS2$country <- c
+    TS2$continent <- continent_c
+    TS2$original <- 0
+    TS2_sample <- TS2[sample(nrow(TS2),(10-c_samp)),]
+    
+    SoilData_smote <- rbind(SoilData_smote[,1:(ncol(SoilData_smote)-1)],TS2_sample[,colnames(SoilData_smote)[1:(ncol(SoilData_smote)-1)]])
+}
+
+table(SoilData_smote$country,SoilData_smote$original)
+
+set.seed(18)
+trainFolds <-  createFolds(SoilData_smote$country, k = 5, returnTrain = T)
+GeoPredsSoil <- list()
+
+for (i in 1:5){
+  
+  train <- SoilData_smote[trainFolds[[i]],]
+  test <- SoilData_smote[-trainFolds[[i]],]
+  
+  testPreds <- mGPS(training = train, testing = test, classTarget = "country",variables = optVars,nthread = 8,hierarchy = c('continent','country','latitude','longitude'), coast=coastlines)
+  GeoPredsSoil[[i]] <- testPreds
+}
+
+add_preds <- list()
+for (i in 1:5){
+  
+  add_preds[[i]] <- cbind(SoilData_smote[-trainFolds[[i]],] , 
+                          "countryPred"= GeoPredsSoil[[i]][[1]], 
+                          "latPred" = GeoPredsSoil[[i]][[2]], 
+                          "longPred" = GeoPredsSoil[[i]][[3]])
+}
+
+SoilDataPreds_smote <- rbind.fill(add_preds)
+write.csv(SoilDataPreds_smote,"soil_results_SMOTE.csv")
+# original samples are labelled as (SoilDataPreds_smote$original == 1)
